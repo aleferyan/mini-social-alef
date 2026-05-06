5a3ae39c3d40f444c749632fb7756d4480b4727f
aleferyan <alefe.7624369@aluno.mg.gov.br>
2026-05-06 14:16:35 +0000

    UPDATE ATUALIZAR TELA

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
-//=== ESTADO (dados da manipulação) ===
+//=== ESTADO (dados da aplicação) === 
+
 let likeCount = 0;
 let dislikeCount = 0;
 let curtido = false;
 let descurtido = false;
 
-//=== SERVICE (regras de negócio) ===
+//=== SERVICE (regras de negócio) === 
 
 function curtir() {
   if (curtido == false){
     likeCount++;
     curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
 
     if(descurtido == true){
       dislikeCount--;
       descurtido = false;
-      document.getElementById("dislikeCount").innerText = dislikeCount;
-
     }
 
   }else{
     likeCount--;
     curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
   }
 
 }
@@ -31,35 +28,41 @@ function descurtir() {
   if(descurtido == false){
     dislikeCount++;
     descurtido = true;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
 
     if(curtido == true){
       likeCount--;
       curtido = false;
-      document.getElementById("likeCount").innerText = likeCount;
     }
 
   }
   else{
     dislikeCount--;
     descurtido = false;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
   }
 }
 
-// === CONTROLLER (intermediação) ===
+// === VIEW (interface/renderização)===
+function atualizarTela(){
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
+}
+
+//=== CONTROLLER (intermediação)===
+
 function clicarCurtir(){
-   curtir();
-
-} 
-
+  curtir();
+  atualizarTela();
+}
 function clicarDescurtir(){
-   descurtir();
-
+  descurtir();
+  atualizarTela();
 }
 
 // === EVENTOS ===
 
-
 document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
 document.getElementById("dislikeBtn").addEventListener("click", clicarDescurtir);
+
+// === INICIALIZAÇÃO ===  
+
+atualizarTela();
